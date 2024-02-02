ed93772e7fb340b222c434563c02f8e80e8115cb
Gibson Han <66423127+gibsonliketheguitar@users.noreply.github.com>
2024-02-01 18:55:00 -0700

    fix button shape gradient and transfmration

--- src/switch/switch-button.jsx
+++ src/switch/switch-button.jsx
@@ -44,12 +44,17 @@ export default function SwitchButton(props) {
             icon={<GridIcon />}
           />
           <Switch.Thumb className={twJoin(
-            "absolute left-0 w-[14px] h-[14px] border-solid border-[1px] shadow rounded-full",
-            "transition-transform translate-x-[1px] will-change-transform data-[state=checked]:translate-x-[21px] duration-500 ease-out",
+            "absolute left-0 flex justify-center items-center w-[14px] h-[14px] rounded-full",
+            "transition ease-out duration-300 data-[state=checked]:w-[10px] data-[state=checked]:rounded-l-[10px] data-[state=checked]:rounded-r-[25px]",
+            "transition-transform translate-x-[1px] will-change-transform data-[state=checked]:translate-x-[25px] duration-500 ease-out",
             color === DEFAULT_COLOR
-              ? 'bg-dark-blue border-light-blue shadow-custom-blue'
-              : 'bg-dark-red border-light-red shadow-custom-red'
+              ? 'bg-gradient-to-tr from-light-blue to-dark-blue shadow-custom-blue'
+              : 'bg-gradient-to-tr from-light-red to-dark-red shadow-custom-red'
           )}>
+            <div className={twJoin(
+              'w-[12px] h-[12px] bg-gradient-to-tr from-dark-blue to-light-blue rounded-full',
+               toggle === LIST && 'transition duration-300 ease-out w-[8px] rounded-l-[8px] rounded-r-[20px]'
+            )}/>
           </Switch.Thumb>
           <Icon
             id='list'
